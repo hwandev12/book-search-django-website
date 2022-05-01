254ee137e71006e903dff18dc16fe89ee2531462
hwandev12 <husan.ahmedov777@yandex.com>
2022-05-02 00:02:06 +0500

    migration files

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('searchfunc.urls'))
 ]
 
