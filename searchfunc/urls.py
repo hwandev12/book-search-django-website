17a802a1a27062871e42677f406c27bc52e21852
hwandev12 <husan.ahmedov777@yandex.com>
2022-05-01 21:58:10 +0500

    create book detail url

--- searchfunc/urls.py
+++ searchfunc/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import *
 
+app_name = 'book'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('<int:pk>/details', bookDetails, name='details')
 ]
