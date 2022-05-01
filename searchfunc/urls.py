b35c7d4b1a912b99404e23224e9f02d29beafcc3
hwandev12 <husan.ahmedov777@yandex.com>
2022-05-01 23:30:08 +0500

    create shop details url

--- searchfunc/urls.py
+++ searchfunc/urls.py
@@ -5,5 +5,6 @@ app_name = 'book'
 
 urlpatterns = [
     path('', home, name='home'),
-    path('<int:pk>/details', bookDetails, name='details')
+    path('<int:pk>/details', bookDetails, name='details'),
+    path('<int:pk>/shop-item', shopDetails, name='shop_details')
 ]
