87c8c4cd177e20f648734c61d583a98c78535df3
hwandev12 <husan.ahmedov777@yandex.com>
2022-05-01 20:26:05 +0500

    create book card model

--- searchfunc/models.py
+++ searchfunc/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# create book card class
+class BookCardsModel(models.Model):
+    class Meta:
+        verbose_name = 'Card Books'
+        verbose_name_plural = 'Card Books'
+    
+    book_cover = models.ImageField()
+    book_name = models.CharField(max_length=100)
+    book_subtitle = models.CharField(max_length=100)
+    book_description = models.TextField(max_length=300)
+    
+    def __str__(self):
+        return str(self.book_name)
