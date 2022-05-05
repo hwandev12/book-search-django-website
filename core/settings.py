1d6a42875040150ca82500baf85f83e78a827f80
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-05 14:30:14 +0500

    migration files

--- core/settings.py
+++ core/settings.py
@@ -110,9 +110,6 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
