c664c756ce5a0f47f489654a3b6a0f799d2e1697
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-05 14:29:39 +0500

    connect slider model

--- searchfunc/admin.py
+++ searchfunc/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import *
 
+# import book models
 admin.site.register(BookCardsModel)
 admin.site.register(BookCategoryModel)
+# import slider models
+admin.site.register(SliderImagesContent)
