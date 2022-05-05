e3756a16535aed9788cbee1daaaf094312def1fe
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-05 14:29:26 +0500

    create slider content

--- searchfunc/models.py
+++ searchfunc/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
+# first page slider model
+class SliderImagesContent(models.Model):
+    class Meta:
+        verbose_name = 'Slider Content'
+        verbose_name = 'Slider Content'
+        
+    slider_img = models.ImageField()
+    slider_header_content = models.CharField(max_length=100)
+    slider_text_content = models.TextField(max_length=400)
+    
+    def __str__(self):
+        return str(self.slider_header_content)
+
+
+
+
+
 # create book card class
 class BookCardsModel(models.Model):
     class Meta:
