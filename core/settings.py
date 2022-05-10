cee37f5f18fda299d1a5884257d63a7114ca9460
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-10 16:25:38 +0500

    crispy form install and configure

--- core/settings.py
+++ core/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'searchfunc',
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,9 @@ MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
