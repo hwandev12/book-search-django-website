b6954232085018b4dc9f2cb86065e9033a686467
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-10 18:49:46 +0500

    create login view url

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static 
+from django.contrib.auth.views import LoginView
 from searchfunc.views import SignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', include('searchfunc.urls')),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
 
 
