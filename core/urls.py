57a43adef4c8fe179a8b6de7e68a09f7181325ba
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-10 16:25:05 +0500

    add sign up url

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static 
+from searchfunc.views import SignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    path('', include('searchfunc.urls'))
+    path('', include('searchfunc.urls')),
+    path('signup/', SignupView.as_view(), name='signup'),
 ]
 
 
