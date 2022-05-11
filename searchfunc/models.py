e2a37c1b40fa3aa4879241dc6dcdf1e08e7fa702
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-11 08:55:34 +0500

    update user profile images

--- searchfunc/models.py
+++ searchfunc/models.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from django.db import models
 from .validators import validate_file_extension
 from django.contrib.auth.models import AbstractUser
@@ -24,6 +25,16 @@ class Profile(models.Model):
 
     avatar = models.ImageField()
     bio = models.TextField()
+    
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 100 or img.width > 100:
+            new_img = (100, 100)
+            img.thumbnail(new_img)
+            img.save(self.avatar.path)
 
     def __str__(self):
         return self.user.username
