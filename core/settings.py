2d02bd5d56e6586313b349a98f9cc2d6b0ea84f8
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-11 15:04:01 +0500

    heroku url

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['book-webite-read.herokuapp.com','127.0.0.1']
 
 
 # Application definition
