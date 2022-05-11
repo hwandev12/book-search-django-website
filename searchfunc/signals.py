c4bebbb1fbb649bf9c2c4516aedae307ae0dcb34
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-11 08:56:40 +0500

    migration and media files

--- searchfunc/signals.py
+++ searchfunc/signals.py
@@ -1,5 +1,5 @@
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
+from .models import User
 from django.dispatch import receiver
 
 from .models import Profile
