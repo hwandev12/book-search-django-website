6da875d63ce7b7565228f6cd414430d63d24e9db
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-11 08:56:10 +0500

    create update user form

--- searchfunc/forms.py
+++ searchfunc/forms.py
@@ -1,11 +1,42 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth.models import User
+from .models import Profile
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-# register form here
-class RegisterForm(UserCreationForm):
+
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(
+        required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
     class Meta:
         model = User
-        fields = ("username",)
+        fields = ['username', 'email']
+
+
+class UpdateProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput(
+        attrs={'class': 'form-control-file'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'rows': 5
+    }))
+
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
+
+
+# register form here
+class RegisterForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ("username", )
         field_classes = {'username': UsernameField}
