83631919ef4c48af6b33ce76856ab6fd7ddf1351
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-14 12:52:13 +0500

    password reset view url

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static 
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+)
 from searchfunc.views import SignupView
 
 urlpatterns = [
@@ -11,4 +15,5 @@ urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-reset-view/', PasswordResetView.as_view(), name='password_reset_view')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
